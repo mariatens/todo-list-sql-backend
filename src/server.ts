62cd329632858e3b225735275079a83bd4be202b
Maria <mtens17@gmail.com>
2022-12-13 11:08:57 +0000

    Have client in different format

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config(); //read any .env file(s)
 
-const client = new Client({connectionString: process.env.DATABASE_URL});
+const client = new Client(process.env.DATABASE_URL);
 
 const app = express();
 
